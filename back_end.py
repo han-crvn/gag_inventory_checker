062ca7a65f7842c3516a3aeb110f6bf6cde2e75d
Shane <palomoshanecraven@gmail.com>
2025-06-05 15:57:15 +0800

    Create class for items.

--- back_end.py
+++ back_end.py
@@ -1,6 +1,24 @@
 # Import libraries
+from abc import ABC, abstractmethod
 
 # Create class for basic details (name, status, sold or not).
+class Items(ABC):
+    def __init__(self, name):
+        self.name = name
+        self.status = "available"
+
+    @abstractmethod
+    def get_details(self):
+        pass
+
+    def mark_sold(self):
+        self.status = "sold"
+
+    def get_status(self):
+        return self.status
+
+    def get_name(self):
+        return self.name
 
 # Create class for goods (details, prices, mutations).
 

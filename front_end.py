69dcef135e728c735f75e5b8000dd218375dee12
Shane <palomoshanecraven@gmail.com>
2025-06-05 16:26:02 +0800

    Pseudocode for the front_end.py

---
+++ front_end.py
@@ -0,0 +1,41 @@
+# Import libraries.
+
+# Create class for the GUI of the app.
+
+    # Set main app.
+
+    # Center the window on the screen.
+
+    # Clear widgets from the main frame.
+
+    # Display the main menu options.
+
+    # Display options to choose between goods or pets.
+
+    # List down all goods and allow users to view more details.
+
+    # Show detailed information of the goods.
+
+    # List down all the pets and show details.
+
+    # Display options to add goods or pets.
+
+    # Create form to add new goods.
+
+    # Create form to add new pets.
+
+    # Display options to choose whether to edit goods or pets.
+
+    # List of goods for editing or removing.
+
+    # OptionS to remove or mark a good a sold.
+
+    # Remove good from inventory.
+
+    # List of pets for editing or removing.
+
+    # OptionS to remove or mark a pet as sold.
+
+    # Remove a pet from inventory.
+
+    # Mark a pet as sold.

55a62a104535549bc539a13c82e4ddbcb1da6fd6
Shane <palomoshanecraven@gmail.com>
2025-06-05 15:50:03 +0800

    Pseudocode for this program.

---
+++ back_end.py
@@ -0,0 +1,9 @@
+# Import libraries
+
+# Create class for basic details (name, status, sold or not).
+
+# Create class for goods (details, prices, mutations).
+
+# Create class for pets (details, prices).
+
+# Create class for whole inventory.

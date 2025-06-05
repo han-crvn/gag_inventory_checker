d4b42a4ef5a3e7dd43d7afd8930a82f1152bcc51
Shane <palomoshanecraven@gmail.com>
2025-06-05 16:07:32 +0800

    Create class for whole inventory functions.

--- back_end.py
+++ back_end.py
@@ -49,3 +49,35 @@ class Pet(Items):
         return self.weight
     
 # Create class for whole inventory.
+class Inventory:
+    def __init__(self):
+        self.goods = []
+        self.pets = []
+
+    def add_good(self, name, mutation, price):
+        self.goods.append(Goods(name, mutation, price))
+
+    def add_pet(self, name, weight):
+        self.pets.append(Pet(name, weight))
+
+    def get_goods(self):
+        return self.goods
+
+    def get_pets(self):
+        return self.pets
+
+    def remove_good(self, index):
+        if 0 <= index < len(self.goods):
+            self.goods.pop(index)
+
+    def mark_good_sold(self, index):
+        if 0 <= index < len(self.goods):
+            self.goods[index].mark_sold()
+
+    def remove_pet(self, index):
+        if 0 <= index < len(self.pets):
+            self.pets.pop(index)
+
+    def mark_pet_sold(self, index):
+        if 0 <= index < len(self.__pets):
+            self.pets[index].mark_sold()
